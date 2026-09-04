04b8518d8b328b10939158f78f0b44816dc11ddd
khellafAymene <aymene.0.khellaf@gmail.com>
2026-09-04 15:45:56 +0300

    g

--- un_logo.py
+++ un_logo.py
@@ -26,7 +26,7 @@ df = dataset.copy()
 # 3. إعداد المشكل لحل مشكلة المربعات (تعطيل الـ ligatures)
 reshaper = arabic_reshaper.ArabicReshaper(configuration={
     'delete_harakat': True,
-    'support_ligatures': True
+    'support_ligatures': False
 })
 
 # 4. دالة معالجة النصوص
